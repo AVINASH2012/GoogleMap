39d71493f89595ef012fc368456ef1c82b8d2dcb
AVINASH2012 <45131407+AVINASH2012@users.noreply.github.com>
2024-01-30 18:22:47 +0530

    Add files via upload

---
+++ GoogleMap.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
+import SplunkVisualization from '@splunk/visualizations/common/SplunkVisualization';
+import mapStyleDark from './map-style.json';
+import { LoadingElement, ContainerElement, MapElement,} from './GoogleMapStyles';
+
+const API_KEY = 'AIzaSyAo3qtnws7Z8fx7AJRX_oJW8mBZ-mSXxGk';
+const mapStyleOptions = (defaultOptions) => ({
+    styles: mapStyleDark,
+    ...defaultOptions,
+});
+
+const MyMapComponent = withScriptjs(
+    withGoogleMap((props) => (
+        <GoogleMap
+            defaultZoom={props.defaultZoom}
+            defaultCenter={props.defaultCenter}
+            defaultOptions={mapStyleOptions(props.defaultOptions)}
+        />
+    ))
+);
+
+const CustomMap = ({ options }) => {
+    return (
+        <MyMapComponent
+            googleMapURL= {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${API_KEY}`}
+            loadingElement={<div style={LoadingElement} />}
+            containerElement={<div style={ContainerElement} />}
+            mapElement={<div style={MapElement} />}
+            {...options}
+        />
+    );
+};
+
+CustomMap.config = {
+    dataContract: {},
+    optionsSchema: {},
+    key: 'splunk.CustomMap',
+    name: 'CustomMap',
+};
+
+CustomMap.propTypes = {
+    ...SplunkVisualization.propTypes,
+};
+
+CustomMap.defaultProps = {
+    ...SplunkVisualization.defaultProps,
+};
+
+export default CustomMap;
---
+++ GoogleMapStyles.js
@@ -0,0 +1,13 @@
+const LoadingElement = {
+    height: `100%`,
+};
+
+const ContainerElement = {
+    height: `calc(100vh - 10px)`,
+};
+
+const MapElement = {
+    height: `100%`,
+};
+
+export { LoadingElement, ContainerElement, MapElement };
