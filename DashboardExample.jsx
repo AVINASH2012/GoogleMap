3b0803326289c76caf14170b3983dc9aff4d235d
AVINASH2012 <45131407+AVINASH2012@users.noreply.github.com>
2024-01-30 19:00:06 +0530

    Add files via upload

---
+++ DashboardExample.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import DashboardCore from '@splunk/dashboard-core';
+import {
+    DashboardContextProvider,
+    GeoRegistry,
+    GeoJsonProvider,
+} from '@splunk/dashboard-context';
+import SplunkThemeProvider from '@splunk/themes/SplunkThemeProvider';
+import EnterpriseViewOnlyPreset from '@splunk/dashboard-presets/EnterpriseViewOnlyPreset';
+import CustomMap from '@splunk/google-map';
+import definition from './definition.json';
+
+const themeToVariant = {
+    enterprise: { colorScheme: 'light', family: 'enterprise' },
+    enterpriseDark: { colorScheme: 'dark', family: 'enterprise' },
+    prisma: { colorScheme: 'dark', family: 'prisma' },
+};
+// use DashboardCore to render a simple dashboard
+
+const geoRegistry = GeoRegistry.create();
+geoRegistry.addDefaultProvider(new GeoJsonProvider());
+
+const customPreset = {
+    ...EnterpriseViewOnlyPreset,
+    visualizations: {
+        ...EnterpriseViewOnlyPreset.visualizations,
+        'splunk.CustomMap': CustomMap,
+    },
+};
+
+const onItemsSelect = () => { 
+    console.log('ok');
+};
+const DashboardExample = () => {
+    return (
+        <SplunkThemeProvider {...themeToVariant.prisma}>
+            <DashboardContextProvider
+                geoRegistry={geoRegistry}
+                preset={customPreset}
+                initialDefinition={definition}
+                onItemsSelect = {onItemsSelect}
+
+            >
+                <DashboardCore width='100%' height='100%' />
+            </DashboardContextProvider>
+        </SplunkThemeProvider>
+    );
+};
+
+export default DashboardExample;
---
+++ index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import layout from '@splunk/react-page';
+import DashboardExample from './DashboardExample';
+
+layout(<DashboardExample />, {
+    pageTitle: 'Map Dashboard',
+    hideFooter: true,
+    layout: 'fixed',
+});
